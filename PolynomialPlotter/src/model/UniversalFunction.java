59079026be612b2bd25c0d380e576b192559059e
Lukas-EppleDHBW <i20009@hb.dhbw-stuttgart.de>
2021-06-15 19:57:44 +0200

    .gitignore and exp4j import

--- PolynomialPlotter/src/model/UniversalFunction.java
+++ PolynomialPlotter/src/model/UniversalFunction.java
@@ -1,11 +1,20 @@
 package model;
 
+import net.objecthunter.exp4j.*;
+
 public class UniversalFunction implements IFunction {
 
+	private String functionalTerm;
+	
+	public UniversalFunction(String term){
+		functionalTerm = term;
+	}
+	
 	@Override
 	public double[] calculate(double start, double end, double step) {
-		// TODO TV LE implement
+		int valueCount = (int) ((end - start) * step);
+		
+		Expression functionExpression = new ExpressionBuilder(functionalTerm).build();
 		return null;
 	}
-
 }
